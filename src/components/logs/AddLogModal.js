615721f590d64924875d8006f45a3581a53cc3f9
Yanni Fraga <yanni_fernanda@hotmail.com>
2019-07-01 22:18:26 -0300

    Added redux

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { addLog } from '../../actions/logActions';
+
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
 	const [ message, setMessage ] = useState('');
 	const [ attention, setAttention ] = useState(false);
 	const [ tech, setTech ] = useState('');
@@ -11,7 +16,16 @@ const AddLogModal = () => {
 			M.toast({ html: 'Please enter a message and tech' });
 		}
 		else {
-			console.log('Submit');
+			const newLog = {
+				message,
+				attention,
+				tech,
+				date: new Date(),
+			};
+
+			addLog(newLog);
+
+			M.toast({ html: `Log added by ${tech}` });
 
 			// clear fields
 			setMessage('');
@@ -80,9 +94,13 @@ const AddLogModal = () => {
 	);
 };
 
+AddLogModal.propTypes = {
+	addLog: PropTypes.func.isRequired,
+};
+
 const modalStyle = {
 	width: '70%',
 	// height: '70%',
 };
 
-export default AddLogModal;
+export default connect(null, { addLog })(AddLogModal);
--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
+import { connect } from 'react-redux';
 
 const LogItem = ({ log }) => {
 	return (
